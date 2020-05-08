4009bae7d400310f4d3a677a487e1d7cc97dff06
QkForce <nurbergen.kuanysh@gmail.com>
2020-05-08 07:29:13 +0600

    Supplements: 1) add some code in index.js 2) add a new data file

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 const fs = require('fs');
+
+
+function main() {
+    const data = {
+        name: "Some name",
+        phone: "8 (747) 487 48 48",
+        location: {
+            address: "Some address (street, house number)",
+            lng: 76.484848,
+            lat: 46.155454
+        }
+    };
+
+    fs.writeFileSync('data.json', JSON.stringify(data));
+}
+
+
+main();
